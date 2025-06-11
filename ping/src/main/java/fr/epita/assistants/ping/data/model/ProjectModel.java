d69502427005c9e4a0d2c2d28de53d903aef92a9
maxime.danion <maxime.danion@epita.fr>
2025-06-11 13:25:31 +0200

    fix(data/model): complete model

--- ping/src/main/java/fr/epita/assistants/ping/data/model/ProjectModel.java
+++ ping/src/main/java/fr/epita/assistants/ping/data/model/ProjectModel.java
@@ -1,8 +1,35 @@
 package fr.epita.assistants.ping.data.model;
 
-import jakarta.persistence.Table;
+import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
 @Table(name = "projects")
+@Getter
+@Setter
 public class ProjectModel {
-    // TODO
+
+    @Column(name = "name")
+    String name; // varchar(255)
+
+
+    @Column(name = "path")
+    String path; // varchar(255)
+
+
+    @Column(name = "owner_id")
+    UserModel owner_id; // uuid (Foreign key)
+
+    @Id
+    @Column(name = "id") @GeneratedValue(strategy = GenerationType.IDENTITY)
+    UUID id;
+
 }
