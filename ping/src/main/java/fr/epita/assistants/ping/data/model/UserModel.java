eb91dd1ffbc8fad2b0970c4c15f5abbdaa0295c6
maxime.danion <maxime.danion@epita.fr>
2025-06-11 13:25:31 +0200

    fix(data/model): complete model

--- ping/src/main/java/fr/epita/assistants/ping/data/model/UserModel.java
+++ ping/src/main/java/fr/epita/assistants/ping/data/model/UserModel.java
@@ -1,8 +1,40 @@
 package fr.epita.assistants.ping.data.model;
 
-import jakarta.persistence.Table;
+import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class UserModel {
-    // TODO
+    
+    @Column(name = "avatar")
+    String avatar; // varchar(255)
+
+    @Column(name = "display_name")
+    String display_name; // varchar(255)
+
+    @Column(name = "is_admin")
+    boolean is_admin; // boolean
+
+    @Column(name = "login")
+    String login; // varchar(255)
+
+    @Column(name = "password")
+    String password; // varchar(255)
+
+    @Id
+    @Column(name = "id") @GeneratedValue(strategy = GenerationType.IDENTITY)
+    UUID id;
+
+    
 }
