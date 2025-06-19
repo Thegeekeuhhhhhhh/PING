fdebd85d38da09bb91648763f0d9d87650aa3565
LEO LE GOAT <leo.menaldo@epita.fr>
2025-06-20 01:19:24 +0200

    Project service ok

--- ping/src/main/java/fr/epita/assistants/ping/domain/service/ProjectService.java
+++ ping/src/main/java/fr/epita/assistants/ping/domain/service/ProjectService.java
@@ -1,6 +1,10 @@
 package fr.epita.assistants.ping.domain.service;
 
+import java.io.File;
 import java.util.*;
+
+import org.apache.commons.io.FileUtils;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import fr.epita.assistants.ping.data.repository.ProjectRepository;
@@ -14,15 +18,61 @@ public class ProjectService {
     @Inject
     ProjectRepository projectRepository;
 
-    public List<ProjectModel> GetProjects() {
-        return projectRepository.GetProjects();
+    @ConfigProperty(name = "PROJECT_DEFAULT_PATH")
+    String path;
+
+    public void createDirectory(UUID id) {
+        if (id == null) {
+            System.out.println("PAS D ID"); // On prend pas de risques
+            return;
+        }
+        new File(path + "/" + id.toString()).mkdirs();
     }
 
-    public ProjectModel AddProject(String name, UserModel owner) {
-        return projectRepository.AddProject(name, owner);
+    public void modifyDirectoryName(UUID oldId, UUID newId) {
+        if (oldId == null || newId == null) {
+            System.out.println("PAS D ID"); // On prend pas de risques
+            return;
+        }
+        new File(path + "/" + oldId.toString()).renameTo(new File(path + "/" + newId.toString()));
     }
 
-    public List<ProjectModel> GetUserProjects(UUID id) {
-        return projectRepository.GetUserProjects(id);
+    public void deleteDirectory(UUID id) {
+        if (id == null) {
+            System.out.println("PAS D ID"); // On prend pas de risques
+            return;
+        }
+        try {
+            FileUtils.deleteDirectory(new File(path + "/" + id.toString()));
+        } catch (Exception e) {
+            System.out.println("SOUCI AVEC LE TRUC LA AU SECOURS");
+        }
+    }
+
+    public List<ProjectModel> getProjects() {
+        return projectRepository.getProjects();
+    }
+
+    public ProjectModel getProject(UUID id) {
+        return projectRepository.getProject(id);
+    }
+
+    public ProjectModel addProject(String name, UserModel owner) {
+        createDirectory(owner.id);
+        return projectRepository.addProject(name, owner);
+    }
+
+    public List<ProjectModel> getUserProjects(UUID id) {
+        return projectRepository.getUserProjects(id);
+    }
+
+    public ProjectModel updateProject(String name, UUID oldId, UUID newId) {
+        ProjectModel temp = projectRepository.updateProject(name, oldId, newId);
+        modifyDirectoryName(oldId, temp.id);
+        return temp;
+    }
+
+    public void deleteProject(UUID id) {
+        projectRepository.deleteProject(id);
     }
 }
